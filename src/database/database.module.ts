87b867cab11141eb8030325fb4666e4de59ebdba
Jontonio <joseantoniorsystem@gmail.com>
2023-05-30 15:24:31 -0500

    30-05-2023

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -15,7 +15,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
           password: configService.get('DB_PASSWORD'),
           database: configService.get('DB_NAME'),
           entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-          synchronize: true, // don't use on production
+          synchronize: false, // don't use on production
         })
       }) 
     ]
